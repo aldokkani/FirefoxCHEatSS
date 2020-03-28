d44e16d5608aead4d1bf680d8d91d9256624fe1a
Hossam Aldokkani <hossam.aldokkani@gmail.com>
2020-03-28 15:47:38 +0200

    Engine logs best move

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 console.log('hello main');
 (function() {
+    let sf;
+    
+    fetch('https://nmrugg.github.io/kingdom/js/stockfish6.js').then(res => {
+        console.log('status ===> ', res.status);
+        res.text().then(data => {
+            console.log('data ===> ', data.length);
+            sf = new Worker(URL.createObjectURL(new Blob([data], { type: 'application/javascript' })));
+            sf.onmessage = function onmessage({ data }) {
+                if (data.indexOf('bestmove') > -1) {
+                    console.log(data);
+                }
+            };
+        });
+    });
+    function calculate(fen) {
+        if (sf !== undefined) {
+            sf.postMessage('position fen ' + fen);
+            sf.postMessage('go ponder');
+
+            setTimeout(function() {
+                sf.postMessage('stop');
+            }, 1000);
+        }
+    }
+    // ========================================================== //
+
     var OrigWebSocket = window.WebSocket;
     var callWebSocket = OrigWebSocket.apply.bind(OrigWebSocket);
     var wsAddListener = OrigWebSocket.prototype.addEventListener;
@@ -22,8 +48,8 @@ console.log('hello main');
             // TODO: Do something with event.data (received data) if you wish.
             const dataObj = JSON.parse(event.data);
             if (dataObj && dataObj.t === 'move') {
-                console.log('received ====>', dataObj.t);
-                // engine(dataObj.t.fen)
+                // console.log('received ====>', dataObj.d);
+                calculate(dataObj.d.fen);
             }
         });
         return ws;
@@ -37,7 +63,7 @@ console.log('hello main');
         // TODO: Do something with the sent data if you wish.
         const dataObj = JSON.parse(data);
         if (dataObj && dataObj.t === 'move') {
-            console.log('sent ====>', dataObj.t);
+            // console.log('sent ====>', dataObj);
         }
         return wsSend(this, arguments);
     };
